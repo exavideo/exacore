97b29f93f75db5d6287cd7602f0907b56c0ea338
Andrew Armenia <andrew@asquaredlabs.com>
2011-03-28 22:29:40 -0400

    Add some explanatory comments to test.rb

--- keydaemon/ruby/test.rb
+++ keydaemon/ruby/test.rb
@@ -3,24 +3,28 @@ include Keyer
 include Driver
 
 keyer do |k|
+    # Set up input adapter to take input from first DeckLink card
     k.input(
         decklink_input do |inp|
             inp.card_index 0
         end
     )
 
+    # Set up output adapter to send to the second DeckLink card
     k.output(
         decklink_output do |out|
             out.card_index 1
         end
     )
 
+    # Generate dynamic scoreboard using scoreboard_cg.rb.
     k.cg(
         svg_subprocess_cg do |cg|
             cg.command '(cd ../..; scoreboard/scoreboard_cg.rb)'
         end
     )
 
+    # Also mix in a static SVG file.
     k.cg(
         svg_subprocess_cg do |cg|
             cg.command '(cd ../..; scoreboard/static_svg.rb)'
