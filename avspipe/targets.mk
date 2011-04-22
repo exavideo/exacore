0382459b386940ee44776079d35a4cc17065112c
Andrew Armenia <andrew@asquaredlabs.com>
2011-04-22 15:55:11 -0400

    Add avpinput_decklink program
    This pipes audio and video input from a DeckLink capture card to some
    client program via a pair of file descriptors.

---
+++ avspipe/targets.mk
@@ -0,0 +1,12 @@
+avspipe_avpinput_decklink_OBJECTS = \
+	$(common_OBJECTS) \
+	$(raw_frame_OBJECTS) \
+	$(drivers_decklink_OBJECTS) \
+	$(thread_OBJECTS) \
+	avspipe/avpinput_decklink.o
+
+avspipe/avpinput_decklink: $(avspipe_avpinput_decklink_OBJECTS)
+	$(CXX) $(LDFLAGS) -o $@ $^ -ljpeg -ldl -pthread
+
+all_TARGETS += avspipe/avpinput_decklink    
+
