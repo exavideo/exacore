26e6f58dc4ba9101bf1c5c96276ff699b0601859
Andrew Armenia <andrew@asquaredlabs.com>
2012-01-27 12:44:12 -0500

    SSE2-enable the down-scaler for previews
    Chroma is a bit off but it's usable...

--- tests/targets.mk
+++ tests/targets.mk
@@ -63,6 +63,16 @@ tests/CbYCrY8422_alpha_BGRAn8: $(test_CbYCrY8422_alpha_BGRAn8_OBJECTS)
 
 all_TARGETS += tests/CbYCrY8422_alpha_BGRAn8    
 
+test_CbYCrY8422_BGRAn8_scale_1_4_OBJECTS = \
+	$(common_OBJECTS) \
+	$(raw_frame_OBJECTS) \
+	tests/CbYCrY8422_BGRAn8_scale_1_4.o
+
+tests/CbYCrY8422_BGRAn8_scale_1_4: $(test_CbYCrY8422_BGRAn8_scale_1_4_OBJECTS)
+	$(CXX) $(LDFLAGS) -o $@ $^
+
+all_TARGETS += tests/CbYCrY8422_alpha_BGRAn8    
+
 test_stretch_OBJECTS = \
 	$(common_OBJECTS) \
 	$(raw_frame_OBJECTS) \
